1b3b7081ac6beecd04e7b15568302becc7cb8e59
Adam Weil <adamweil@Adams-MBP.lan1>
2020-12-01 00:09:48 -0500

    button to bring back to start

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,13 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
     // var aBool = true;
     // aBool = false;
@@ -94,7 +101,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     String resultText;
@@ -20,11 +22,18 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(resultPhrase,
-          style: TextStyle(
-            fontSize: 36,
-            fontWeight: FontWeight.bold,
-          )),
+      child: Column(
+        children: <Widget>[
+          Text(resultPhrase,
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center),
+              FlatButton(
+                child: Text(
+                  'Restart Quiz',
+              ), 
+              textColor: Colors.blue, onPressed: resetHandler),
+        ],
+      ),
     );
   }
 }
