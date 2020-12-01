77f84d2e263b6116305f830aee08bd25b9777ade
Adam Weil <adamweil@Adams-MBP.lan1>
2020-11-30 22:02:48 -0500

    including result widget

--- lib/main.dart
+++ lib/main.dart
@@ -25,22 +25,41 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What\'s is your fav color????',
-      'answers': ['Black', 'Red', ' Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 3},
+        {'text': 'Green', 'score': 3},
+        {'text': 'Blue', 'score': 3},
+      ],
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Dogs', 'Cats', 'Sheep', 'Birds'],
+      'answers': [
+        {'text': 'Dogs', 'score': 100},
+        {'text': 'Cats', 'score': -10},
+        {'text': 'Sheep', 'score': 6},
+        {'text': 'Birds', 'score': 66},
+      ],
     },
     {
       'questionText': 'What\'s your gender?',
-      'answers': ['Male', 'Female', 'Neither', 'Both']
+      'answers': [
+        {'text': 'Male', 'score': 2},
+        {'text': 'Female', 'score': 3},
+        {'text': 'Neither', 'score': 4},
+        {'text': 'Both', 'score': 5},
+      ],
     }
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
     // var aBool = true;
     // aBool = false;
+
+    // _totalScore = _totalScore + score;
+    _totalScore += score;
 
     setState(() {
       _questionIndex = _questionIndex + 1;
@@ -70,17 +89,15 @@ class _MyAppState extends State<MyApp> {
           title: Text('My First App'),
         ),
         body: _questionIndex < _questions.length
-        ? Quiz(
-            answerQuestion: _answerQuestion, 
-            questionIndex: _questionIndex,
-            questions: _questions, 
-          )
-        : Center(
-            child: Text('You did it!'),
-        ),
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore),
       ),
     );
   }
 }
 
-// Session 46 on Udemy
+// Session 53 on Udemy
